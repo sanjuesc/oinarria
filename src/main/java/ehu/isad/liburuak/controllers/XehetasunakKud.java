41062f38dc36db817d00b685d4c6ddfc697bf1e1
ander <elsanjuyt@gmail.com>
2020-10-20 08:49:16 +0200

    argazkiak gorde eta bistaratzen ditu, path-a aldatzea falta da

--- src/main/java/ehu/isad/liburuak/controllers/XehetasunakKud.java
+++ src/main/java/ehu/isad/liburuak/controllers/XehetasunakKud.java
@@ -8,15 +8,21 @@ import ehu.isad.liburuak.utils.Sarea;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.effect.ImageInput;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.ResultSet;
+
 
 public class XehetasunakKud {
 
@@ -60,6 +66,8 @@ public class XehetasunakKud {
             izenburuText.setText(xehe.getString("title"));
             argitalText.setText(xehe.getString("publisher"));
             orriKopText.setText(String.valueOf(xehe.getInt("pages")));
+            Image Image = new Image(new FileInputStream(isbn+".png"));
+            irudiaField.setImage(Image);
         }else{
             book = this.getLib(isbn);
             Details details = book.getDetails();
@@ -67,16 +75,22 @@ public class XehetasunakKud {
             izenburuText.setText(title);
             String publisher = details.getArgitaretxea();
             argitalText.setText(publisher);
+            publisher = details.getArgitaretxea().replace("\'","\'\'"); //publisher moldatu datu baserako
             String pages =String.valueOf(details.getPages());
             orriKopText.setText(pages);
             String query = "insert into Xehetasunak values('"+isbn+"','"+publisher+"','"+pages+"','"+title+"');";
             System.out.println(query);
             DBKudeatzaile dbKudeatzaile = DBKudeatzaile.getInstantzia();
             ResultSet rs = dbKudeatzaile.execSQL(query);
+            String url = book.getThumbnail_url().replace("S", "L");
+            Image i = createImage(url);
+            URL Url = new URL(url);
+            BufferedImage image = ImageIO.read(Url);
+            File outputfile = new File(isbn+".png");
+            ImageIO.write(image, "png", outputfile);
+            irudiaField.setImage(i);
         }
-        //String url = book.getThumbnail_url().replace("S", "L");
-        //Image i = createImage(url);
-        //irudiaField.setImage(i);
+
     }
 
     private Image createImage(String url) throws IOException {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,6 @@ open module oinarria {
   requires javafx.controls;
   requires javafx.fxml;
     requires com.google.gson;
+    requires java.desktop;
     exports ehu.isad.liburuak;
 }
